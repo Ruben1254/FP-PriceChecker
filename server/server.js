fca54c93957e8234d35ffb5878b2c835a983d262
Ruben1254 <reuven.tsbn@gmail.com>
2022-10-28 01:07:27 +0300

    5000

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ dotenv.config();
 
 const app = express();
  
-app.listen(9197, () => {
-    console.log(`run on port 9197`)
+app.listen(5000, () => {
+    console.log(`run on port 5000`)
 });
 
 app.use(cors());
